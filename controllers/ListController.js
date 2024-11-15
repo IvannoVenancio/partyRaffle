fafc16b4de4f564ef5d2c700745acec74b967433
Ivanno <ijvenancio@cetim.ms>
2024-11-15 22:57:30 +0100

    changes

--- controllers/ListController.js
+++ controllers/ListController.js
@@ -17,7 +17,7 @@ exports.create = async(req, res) => {
         const data = req.body
         await createList(data)   
         const list = await getList()    
-        res.render("list", {list})  
+        res.render("list", {layout:'listBase',list})  
     }catch(err){
         res.redirect("index")
     }      
